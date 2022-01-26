45a351fb6c7c7b7da9590e46e23ad1ff8e03fdc9
Guilherme Bispo <guialbquerque@gmail.com>
2022-01-25 22:53:51 -0300

    Created the child classes where there is specific attributes, like duration and season

--- playlist_fina.py
+++ playlist_fina.py
@@ -15,3 +15,27 @@ class Program:
     @property
     def likes(self):
         return f' Likes: {self._likes}'
+
+class Movies(Program):
+    def __init__(self, name, year, likes, duration):
+        super().__init__(name, year, likes)
+        self._duration = duration
+    
+    @property
+    def duration(self):
+        return f'Duration: {self._duration} min'
+    
+    def __str__(self):
+        return f'Name: {self._name}\nYear: {self._year}\nDuration: {self._duration} min\nLikes: {self._likes}\n'
+
+class Series(Program):
+    def __init__(self, name, year, likes, season):
+        super().__init__(name, year, likes)
+        self._season = season
+    
+    @property
+    def season(self):
+        return f' Seasons: {self.season}'
+
+    def __str__(self):
+        return f'Name: {self._name}\nYear: {self._year}\nSeasons: {self._season}\nLikes: {self._likes}\n'
