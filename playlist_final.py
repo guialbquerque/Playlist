4b57a988e5f9f5366f77b78e711baacb26da6427
Guilherme Bispo <guialbquerque@gmail.com>
2022-01-25 23:15:26 -0300

    Created how the playlist will catch the necessary informations and finally how it will save in txt.file

--- playlist_final.py
+++ playlist_final.py
@@ -49,3 +49,63 @@ class Playlists:
         print('=*'*30)
         return self._programs[item]
 
+Film = []
+Serie = []
+
+number_films = input('How many movies are there in the playlist?\n')
+while number_films.isnumeric() == False:
+
+    number_films = input('Please, type a numerical argument: \n')
+
+
+counter = 0
+likes = 0
+while counter < int(number_films):
+    
+    
+    for i in range(1, int(number_films)+1):
+        counter += 1
+        year = input(f'Year of the {i}º movie:\n')
+        while year.isnumeric() == False or year > str(2021):
+            counter = i 
+            year = input(f'Wrong type of movie {i}, type a valid year:\n')
+        name = input(f'Name of the {i}º movie:\n').capitalize()
+        duration = input(f'Duration of the {i}º movie in minutes:\n')
+        likes = input(f'Likes of the {i}º movie:\n')
+        Film.append(Movies(name,year, likes, duration))
+
+counter2 = 0
+
+number_series = input('How many series are there in the playlist?\n')
+while number_series.isnumeric() == False:
+
+    number_series = input('Please, type a numerical argument: \n')
+
+while counter2 < int(number_series):
+    counter2 += 1
+for i in range(1, int(number_series)+1):
+    year = input(f'Year of the {i}º serie:\n')
+    while year.isnumeric() == False or year > str(2021):
+        counter2 = i
+        year = input(f'Wrong type of serie {i}, type a valid year:\n')
+    series = input(f'Name of the {i} serie:\n').capitalize()
+    seasons = input('Number of seasons:\n')
+    likes_serie = input(f'Likes of the {i}º serie:\n')
+    Serie.append(Series(series, year, likes_serie, seasons))
+    
+            
+
+PlaylistFinal = Film
+PlaylistFinal.extend(Serie)
+
+print('=*'*30)
+
+print('Final Playlist: \n')
+print('\n')
+Weekend =  Playlists('Weekend of Fun', PlaylistFinal)
+
+for i in Weekend:
+    print(i)
+    with open('Playlist.txt', 'a') as output:
+        output.write(str(i))
+
